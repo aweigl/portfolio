04c5dc1f1591956d3d23688b13c03e121a2307cf
Aaron Weigl <aaron.weigl@hotmail.com>
2018-05-24 14:08:24 +0200

    case sensitivity changed

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ class Main extends React.Component {
                 <img alt="" src="/images/Koeln/KoelnKoelnerWirMain.jpg" />
               </Link>{" "}
               <Link to="/stellland">
-                <img alt="" src="/images/Stellland/StelllandMain.jpg" />
+                <img alt="" src="/images/Stellland/STELLLANDMAIN.jpg" />
               </Link>{" "}
               <Link to="dirak">
                 <img alt="" src="/images/Dirak/DirakMain.jpg" />
